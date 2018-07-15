2225f3ab2008d076ff745b551c0aaecd3064fee3
Matt Peebles <peebles.mm@gmail.com>
2018-07-14 19:45:30 -0700

    split index.js into modules

---
+++ twitch-commands.js
@@ -0,0 +1,19 @@
+// Function called when the "echo" command is issued:
+function echo (twitchHelper){
+    return function(target, context, params) {
+        // If there's something to echo:
+        if (params.length) {
+            // Join the params into a string:
+            const msg = params.join(' ')
+            
+            // Send it back to the correct place:
+            twitchHelper(target, context, msg)
+        } else { // Nothing to echo
+            console.log(`* Nothing to echo`)
+        }
+    }
+}
+
+module.exports = {
+    echo
+}
