8166470f6eb7c9825b52395a4c8dfa8cc792c63a
Matt Peebles <peebles.mm@gmail.com>
2018-07-14 19:45:09 -0700

    Added database connection file

---
+++ db.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+module.exports = {
+    load(){
+        var connection = mysql.createConnection({
+            host     : process.env.RDS_HOSTNAME,
+            user     : process.env.RDS_USERNAME,
+            password : process.env.RDS_PASSWORD,
+            port     : process.env.RDS_PORT
+          });
+          
+          connection.connect(function(err) {
+            if (err) {
+              console.error('Database connection failed: ' + err.stack);
+              return;
+            }
+          
+            console.log('Connected to database.');
+          });
+          
+          connection.end(err => {
+              if(err){
+                  console.error('Database connection failed to end: ' + err.stack);
+                  connection.destroy();
+                  return;
+              }
+          
+              console.log("Connection to database ended")
+          });
+    }
+}
+
